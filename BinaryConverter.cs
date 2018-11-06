77bbc4540c7e9d83b096c61bc24603d3bd484f24
Andrei Dana <andrei.dana87@gmail.com>
2018-11-06 23:14:46 +0200

    Create binary converter

---
+++ BinaryConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumericConverter
+{
+    public class BinaryConverter
+    {
+        public BinaryConverter() {}
+
+        public string ToBinary(int inputNumber)
+        {
+            var binaryValue = string.Empty;
+
+            for(; inputNumber > 1; inputNumber /= 2)
+                binaryValue += inputNumber % 2;
+
+            binaryValue += inputNumber % 2;
+
+            return Reverse(binaryValue);
+        }
+
+        private string Reverse(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
